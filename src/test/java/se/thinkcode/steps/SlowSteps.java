0f7b3ff8bfa160821ac21aec1dacf61d60743f89
Thomas Sundberg <tsu@kth.se>
2019-02-18 08:33:48 +0100

    Verify that we can see system properties sent to Gradle in the new JVM running Cucumber

--- src/test/java/se/thinkcode/steps/SlowSteps.java
+++ src/test/java/se/thinkcode/steps/SlowSteps.java
@@ -6,6 +6,13 @@ import se.thinkcode.Slow;
 public class SlowSteps {
     @Given("a wanted execution time of {int} seconds")
     public void a_wanted_execution_time_of_seconds(Integer wantedExecutionTime) {
+        String foo = System.getProperty("foo");
+        if (foo != null) {
+            System.out.println("Got the system property 'foo'");
+        } else {
+            System.out.println("Did not get the system property 'foo'");
+        }
+
         Slow slow = new Slow();
 
         long start = System.currentTimeMillis();
